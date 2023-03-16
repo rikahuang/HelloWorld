1838142816ced89cf650acd90b3565596b758e28
rikahuang <chirpchirp.huang@gmail.com>
2023-03-16 18:36:55 -0400

    Created gallery
    Photo gallery WIP

--- PhotosOfMe.js
+++ PhotosOfMe.js
@@ -1,3 +1,36 @@
+var gallery = [
+    {
+        "image": "IMG_2220.jpg",
+        "caption": "flowa powa",
+        "class": "frameLt"
+    },
+    {
+        "image": "IMG_0551.jpg",
+        "caption": "my handsome side profile",
+        "class": "frameRt"
+    },
+    {
+        "image": "IMG_0881.jpg",
+        "caption": "cheese!!!",
+        "class": "frameLt"
+    },
+    {
+        "image": "IMG_1691.jpg",
+        "caption": "me so handsome",
+        "class": "frameRt"
+    },
+    {
+        "image": "IMG_9025.jpg",
+        "caption": "fluffy fluffy snow",
+        "class": "frameLt"
+    },
+    {
+        "image": "IMG_9355.jpg",
+        "caption": "snowdog mochi",
+        "class": "frameRt"
+    },
+];
+
 function openSidePanelV2() {
     var sidePanel = document.getElementById("sidePanel");
     sidePanel.classList.remove("sidePanelHide");
@@ -33,3 +66,51 @@ function calculatorPage() {
 function faqPage() {
     window.open("FAQ.html", "_self");
 }
+
+function loadGallery() {
+    var galleryIndex = 0;
+
+    for (var r=0; r<3; r++) {
+        
+        var row = document.createElement("div");
+        row.classList.add("galleryRow");
+
+        for (var c=0; c<2; c++) {
+            
+            var pic = document.createElement("img");
+            pic.src = gallery[galleryIndex].image;
+            pic.classList.add("imgSize");
+
+            var innerFrame = document.createElement("div");
+            innerFrame.classList.add("picFrameInner");
+
+            innerFrame.appendChild(pic);
+
+            var cap = document.createElement("div");
+            cap.classList.add("caption");
+            cap.innerHTML = gallery[galleryIndex].caption;
+
+            var outerFrame = document.createElement("div");
+            outerFrame.classList.add("picFrame");
+            outerFrame.classList.add("pointer");
+            outerFrame.classList.add(gallery[galleryIndex].class);
+
+            outerFrame.appendChild(innerFrame);
+            outerFrame.appendChild(cap);
+
+            row.appendChild(outerFrame);
+
+            galleryIndex = galleryIndex + 1;
+            
+        }
+
+        var mainGallery = document.getElementById("MG");
+        mainGallery.appendChild(row);
+
+    }
+}
+
+window.addEventListener("load", function(){
+    loadGallery();
+})
+
